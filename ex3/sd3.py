727010160626528e55a6aada7a0ed1b869793945
Julio Diaz <juliocdiazramirez@gmail.com>
2017-02-10 21:21:18 -0400

    finished ex3

---
+++ ex3/sd3.py
@@ -0,0 +1,4 @@
+print "Force = mass * acceleration."
+print "So if mass is equal to 14 and accelerations is equal to 9.8"
+print "The Force is equal to", 14.0 * 9.8
+# the comma is necessary after a string to be able to print numbers
