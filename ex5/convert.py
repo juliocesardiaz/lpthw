c55eb0cc1572e685331eefd06cc4cfd7ff77e347
Julio Diaz <juliocdiazramirez@gmail.com>
2017-02-21 04:18:53 -0400

    finished excercise 5

---
+++ ex5/convert.py
@@ -0,0 +1,7 @@
+inch = 1
+pound = 1
+centimeter = 2.54 * inch
+kilogram = 0.453592 * pound
+
+print "There are %f centimeters in %f inch." % (centimeter, inch)
+print "And there is %f kilograms in %f pounds." % (kilogram, pound)
