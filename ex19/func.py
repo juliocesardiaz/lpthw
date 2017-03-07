f326a1b839f04c989411a24088cf11756aeb50aa
Julio Diaz <juliocdiazramirez@gmail.com>
2017-03-07 00:50:26 -0400

    finished exercise 19

---
+++ ex19/func.py
@@ -0,0 +1,17 @@
+def multiply(x, y):
+    print "%d times %d equals %d" % (x, y, x * y)
+
+multiply(10, 64)
+multiply(15, 15)
+multiply(13, 13)
+
+x = 15
+y = 13
+
+multiply(x, y)
+
+multiply(2 + 4, 3 + 2)
+
+multiply(x + 3, y + 200)
+
+multiply(x * x, y * y)
