d0d5f1653d711229132e137a7bc518a61a9e4aa5
Julio Diaz <juliocdiazramirez@gmail.com>
2017-03-10 03:13:45 -0400

    finished exercise 33

---
+++ ex33/ex33.py
@@ -0,0 +1,29 @@
+def looper(x, increment):
+
+    i = 0
+    numbers = []
+
+    while i < x:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i += increment
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+
+    print "The numbers: "
+
+    for num in numbers:
+        print num
+def for_looper(x):
+    numbers = range(x)
+
+    print "The numbers: "
+    for num in numbers:
+        print num
+
+looper(6, 1)
+looper(3, 1)
+looper(20, 2)
+for_looper(6)
