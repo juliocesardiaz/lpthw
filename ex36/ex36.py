bb605a83365fee467812b9e415fd0c280530bf3f
Julio Diaz <juliocdiazramirez@gmail.com>
2017-03-11 04:47:47 -0400

    finished room B of ex36 game

--- ex36/ex36.py
+++ ex36/ex36.py
@@ -19,4 +19,22 @@ def enterance():
             print "That's not a room, try again! Hint: Choose a room by inputting 1-3."
 
 def room_b():
+    print "You enter a room, in it you find a Sphinx."
+    print "Sphinx: \'Hello, answer this riddle and I will let you choose a door, otherwise I will devour you whole.\'"
+    print "Sphinx: \'What walks on four legs in the morning three in the afternoon and two at night?\'"
+
+    answer = raw_input('> ')
+    if answer.lower == "man":
+        print "Sphinx: \'You are correct, choose one of the two doors."
+
+        choice = raw_input('> ')
+        if choice == "1":
+            room_c()
+        elif choice == "2":
+            room_i()
+        else:
+            print "That's not a room, try again! Hint: Choose a room by inputting 1 or 2."
+    else:
+        dead("The sphinx devoured you're head.")
+def room_c():
     
