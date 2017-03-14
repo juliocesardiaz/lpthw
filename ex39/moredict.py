70f562ba9199b852ddb96f9fa7a8050084943a2d
Julio Diaz <juliocdiazramirez@gmail.com>
2017-03-14 02:46:15 -0400

    finished exercise 39

---
+++ ex39/moredict.py
@@ -0,0 +1,20 @@
+d = {
+    'idk' : 'boom',
+    'why' : 'cccc',
+    'pop' : 'crackle',
+    'hip' : 'hop'
+}
+
+print '-' * 20
+print d.keys()
+print '-' * 20
+print d.items()
+print '-' * 20
+print iter(d)
+print '-' * 20
+print d.copy()
+print '-' * 20
+print d.has_key('bip')
+print '-' * 20
+print len(d)
+print '-' * 20
