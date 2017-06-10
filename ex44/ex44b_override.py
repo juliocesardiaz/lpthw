9a8df608f5b2612389e90f6236b061e6cfb4c400
Julio Diaz <juliocdiazramirez@gmail.com>
2017-06-09 20:07:15 -0400

    finished ex44

---
+++ ex44/ex44b_override.py
@@ -0,0 +1,16 @@
+## an example of overriding a function inherinted in the child class
+class Parent(object):
+
+    def override(self):
+        print "PARENT override()"
+
+class Child(Parent):
+
+    def override(self):
+        print "CHILD override()"
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
